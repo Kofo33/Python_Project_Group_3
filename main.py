7d383247f90f2943c68ea35e1bea7bf8359169e5
movs007 <muves218@gmail.com>
2025-08-11 17:29:32 +0100

    created the display.py and included image assets

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from classes.player import Player
+from classes.enemy import Enemy
+from classes.constant import WIDTH, HEIGHT, FPS, SHOOT_DELAY
+from display import display_menu
+# from database import init_db, save_player, load_player
+
+
+def main():
+    pygame.init()
+
+    display_menu()
+   
+
+   
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
