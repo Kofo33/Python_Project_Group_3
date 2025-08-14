ec792c0bf634af36608b8a76e4b461bbd5a89f9e
Kofo33 <idowukofo3@gmail.com>
2025-08-14 19:33:03 +0100

    Fixed the loading of users to the database

--- save_load.py
+++ save_load.py
@@ -47,23 +47,21 @@ def save_game(player, level_count):
     print("💾 Game saved to database.")
 
 
-def load_game():
+def load_game(name):
     init_db()
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM player LIMIT 1")
+    cursor.execute("SELECT * FROM player WHERE name = ?", (name,))
     row = cursor.fetchone()
     conn.close()
 
     if row:
-        player = Player(x=WIDTH//2, y=HEIGHT//2, name=row[0], level=row[1])
-        player.health = row[2]
-        player.attack = row[3]
-        player.xp = row[4]
+        player = Player(250, 370, name=row[0], health=row[2], attack=row[3], level=row[1], xp=row[4])
         player.inventory = json.loads(row[5])
-        print("✅ Game loaded from database.")
+        print(f"✅ Game loaded for {name}.")
         return player, player.level
     else:
-        print("⚠️ No saved game found.")
+        print(f"⚠️ No saved game found for {name}.")
         return None, 1
+
